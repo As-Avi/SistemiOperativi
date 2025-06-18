832d1cd29d3073f7a25dca08116fd97f8ff11536
As-Avi <lorenzoaltomare@tutamail.com>
2025-06-18 15:50:47 +0200

    12

--- fileExe2.c
+++ fileExe2.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <dirent.h>
 #include <sys/types.h>
 #include <string.h>
 #include <errno.h>
--- mmap2.c
+++ mmap2.c
@@ -3,18 +3,20 @@
 #include <unistd.h>
 #include <sys/mman.h>
 #include <fcntl.h> /* Per O_RDWR */
-#include <sys/stat.h> 
+#include <sys/stat.h>
 
 int main(){
     int fd;
     char * mem;
-
     fd = shm_open("mymem", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
     ftruncate(fd, 512);
     mem = (char *) mmap(NULL, 512, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
     while(1){
-        printf("Scrivere: ");
-        scanf("%s", mem);
+        printf("Premi enter per leggere");
+        getchar();
+        printf("Data: %s\n\n", mem);
     }
-    munmap(mem, 512); /* Inutile causa loop infinito*/
+    munmap(mem, 512);
+    return 0;
 }
